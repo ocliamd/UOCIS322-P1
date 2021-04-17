cc7d50db275813eecdfa7b01d0c6e0349fa7c0a2
alih <alihassanijr1998@gmail.com>
2021-04-17 14:34:02 -0700

    Final changes

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 #   examples:
 #  ./start.sh -P 9000
 #            to start server on port 9000
-#  ./start.sh 
-#            to start server on port from configuration files  
-# 
-python3 pageserver/pageserver.py $* & 
+#  ./start.sh
+#            to start server on port from configuration files
+#
+python3 pageserver/pageserver.py $* &
 echo $! >,pypid
-echo "----> Kill with: kill: " `cat ,pypid`
+echo "----> If make stop doesn't work, kill using: kill -9 " `cat ,pypid`
